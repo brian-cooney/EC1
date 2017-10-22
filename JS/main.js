e23eddf64c4dbd49bf60109571ae764bbbc197e8
brian-cooney <bcooney@gmail.com>
2017-10-22 19:46:49 +0100

    Play icon added to video

--- JS/main.js
+++ JS/main.js
@@ -12,3 +12,14 @@ dots: true
     $hamburger.toggleClass("is-active");
     // Do something else, like open/close menu
   });
+
+
+  $('.video').parent().click(function () {
+      if($(this).children(".video").get(0).paused){
+          $(this).children(".video").get(0).play();
+          $(this).children(".playpause").fadeOut();
+      }else{
+         $(this).children(".video").get(0).pause();
+          $(this).children(".playpause").fadeIn();
+      }
+  });
