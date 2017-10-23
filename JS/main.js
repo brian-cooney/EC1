f074507ebe8d0d940c4c2df4f21a03fba2609472
Brian Cooney <bcooney@gmail.com>
2017-10-23 09:56:14 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -10,7 +10,6 @@ dots: true
   var $hamburger = $(".hamburger");
   $hamburger.on("click", function(e) {
     $hamburger.toggleClass("is-active");
-    // Do something else, like open/close menu
   });
 
 
